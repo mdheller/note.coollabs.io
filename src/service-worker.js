e6f47aaaeb6c17ac796d64ae13c08f1598f90eff
Andras Bacsai <andras.bacsai@gmail.com>
2019-12-22 19:22:10 +0100

    Update

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,7 +2,7 @@
 const DEBUG = true
 const { assets } = global.serviceWorkerOption
 const CACHE_NAME = new Date().toISOString()
-let assetsToCache = [...assets, './', '//cdn.coollabs.io/buefy.min.js']
+let assetsToCache = [...assets, './', 'https://cdn.coollabs.io/buefy.min.js']
 assetsToCache = assetsToCache.map(path => {
   return new URL(path, global.location).toString()
 })
