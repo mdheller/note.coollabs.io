0b6075746150396e2cc5f637a53e2a40757cbf0b
Andras Bacsai <andras.bacsai@gmail.com>
2019-12-23 23:34:13 +0100

    Should work now

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,12 @@ module.exports = {
             keyframes: true,
             /* defaultExtractor: content => content.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [], */
             content: ['./public/**/*.html', './src/**/*.vue', './node_modules/@coollabsio/developer-kit/dist/developer-kit.umd.js'],
-            whitelist: ['fade-enter-active', 'fade-leave-active', 'fade-enter', 'fade-leave-to', 'v-lazy-image-loaded']
+            whitelist: ['fade-enter-active', 'fade-leave-active', 'fade-enter', 'fade-leave-to', 'v-lazy-image-loaded'],
+            whitelistPatternsChildren: [
+              /b-tooltip.*cool.*/,
+              /is-cool.*/,
+              /toast.*/
+            ]
           })
         ]
       }
